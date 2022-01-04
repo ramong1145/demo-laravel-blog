b37c1c0cde11d1eb1a65c855c96a59bc25b55e10
Ramon Gallo <ramong1145@gmail.com>
2022-01-03 18:18:55 -0600

    FIXED versioning problem with sluggable library

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\ServiceProvider;
 
 class Post extends Model
 {
     use HasFactory;
+    use ServiceProvider;
+
+     /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title',
+                'onUpdate' => true
+            ]
+        ];
+    }
+
 }
